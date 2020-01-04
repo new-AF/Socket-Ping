efdc4a8b978282067614c17b32174d6300b575c8
Abdullah Fatota <53046436+new-AF@users.noreply.github.com>
2020-01-04 10:44:56 +0100

    + self.collide to Ball class

--- project2.py
+++ project2.py
@@ -1,5 +1,5 @@
 import Tkinter as t
-
+import time
 top = t.Tk()
 def res(e):
     #print (dir(e),e.width,e.height)
@@ -33,13 +33,19 @@ def visible(top):
     top.bind('<Motion>',move)
 
     def click(e):
-        ball = Ball(top,(e.x,e.y))
+        ball = Ball(top,(e.x,e.y),target = (b,b2)  )
     top.bind('<ButtonRelease>',click)
 ##    top2 = t.Toplevel()
 ##    top2.title(top.title())
 c = t.Canvas(top)
 c.pack(side=t.TOP,fill=t.BOTH,expand=1)
 
+##class Active:
+##    def __init__(self,target,criteria):
+##        self.one,self.two = target
+##        self.widget,self.function = criteria
+##    def get(self):
+##        return self.one if self.function(self.widget) else self.two
 class Bar:
     def __init__(self,parent,inset=(-10,-10)):
         self.parent = parent
@@ -72,7 +78,8 @@ class Bar2(t.Button):
         self.pw = 50
         self.ph = 100
         x,y = inset
-        t.Button.__init__(self,parent,**kw)
+
+        t.Button.__init__(self,parent,anchor='nw',**kw)
         if x<0:
             x = top.my_width + x - self.pw
             self['text'] = 'right'
@@ -84,6 +91,11 @@ class Bar2(t.Button):
         self.y = y
 
         self.place(x = x,y = y)
+        self.bind('<Visibility>',self.visible)
+    def visible(self,e):
+
+        self.width,self.height = (self.winfo_width(),self.winfo_height())
+        self.unbind('<Visibility>')
 ##        parent.bind('<Motion>',self.move)
 
 ##    def move(self,e):
@@ -96,8 +108,10 @@ class Bar2(t.Button):
 class Ball(t.Button):
 
     def __init__(self,parent,coordinates = (0,0),**kw):
+
+        self.target = kw.pop('target')
         self.parent = parent
-        t.Button.__init__(self,parent,**kw)
+        t.Button.__init__(self,parent,anchor='nw',**kw)
         self['bitmap'] = 'error'
         self['relief'] = 'flat'
 
@@ -108,12 +122,37 @@ class Ball(t.Button):
         self.moving = 1
         self.start()
 
+    def collide(self,side = 0):
+        target = self.target[side]
+
+        x,y = self.x, self.y
+        w = self.winfo_width()
+        h = self.winfo_height()
+
+        tx,ty = target.winfo_x(), target.winfo_y()
+        th, tw = target.height, target.width
+
+##        if side == 0:
+##            c1 = x + w > tx
+##        elif side ==1:
+##            c1 = x + w < tx+tw
+        c1 = abs((x+w/2) - (tx + tw/2)) <= 5
+        c2 = y + h >= ty and y+h <= ty + th
+
+        return c1 and c2
     def start(self):
         self.x += self.incx
         self.y +=self.incy
-
+##        print (self.target[0]['text'])
+        #print (self.target[0].winfo_x(),self.target[0].winfo_y(),'collision')
+        #target = self.target.get()
         if self.x >= self.parent.my_width or self.x <= 0:
             self.incx *= -1
+
+        if  self.collide(0) or self.collide(1):
+            self.incx *= -1
+            #print (time.clock(),'collision')
+
         if self.y >= self.parent.my_height or self.y <= 0:
             self.incy *= -1
 
